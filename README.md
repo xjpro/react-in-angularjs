868eafae3105fb1dddca9507e660df66cb709f59
Joshua Prodahl <jprodahl@gmail.com>
2019-01-07 12:54:30 -0600

    a way to do two-way bindings

--- README.md
+++ README.md
@@ -36,3 +36,27 @@ TodoList React component now wrapped in an AngularJS component named "todoList".
 ```html
 <todo-list todos="{{todos}}"></todo-list>
 ```
+
+## Caveats
+
+### AngularJS within React
+
+You can't use AngularJS within the React components so you'll need to work bottom up, i.e. replace low level
+components first.
+
+### Two Way Bindings
+
+Two way bindings are not recommended, either by the AngularJS team or by me. However, it's not always possible to
+remove them in a legacy application. In those cases, you will need to apply any changes like this:
+
+```js
+export class TodoItem {
+  // imagine some React component with a change handler
+  onChange = () => {
+    this.props.$$scope.$apply(() => {
+      // $$scope = AngularJS component scope, provided on a prop via react-in-angularjs
+      this.props.twoWayBoundProperty = "new value"
+    });
+  };
+}
+```
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 
+let gaveWarning = false;
+
 const angularize = (Component, angularApp, bindings) => {
 	if (typeof window === "undefined" || typeof angularApp === "undefined") return;
 
 	const componentName = `${Component.name.charAt(0).toLowerCase()}${Component.name.slice(1)}`;
 	angularApp.component(componentName, {
 		bindings,
-		controller: function ($element) {
+		controller: function ($rootScope, $element) {
+			for (let bindingKey in bindings) {
+				if (!gaveWarning && bindings.hasOwnProperty(bindingKey) && bindings[bindingKey] === "=") {
+					console.warn("react-in-angularjs: You are using two-way bindings. This is not recommended. You'll need to apply changes via this.props.$$scope.$apply.");
+					gaveWarning = true;
+				}
+			}
+
+			if (window.angular) {
+				this.$$scope = window.angular.element($element).scope();
+			}
+
 			this.$onChanges = () => {
 				ReactDOM.render(React.createElement(Component, this), $element[0]);
 			};
