64553c47880c8034bc4695242f26c1ded866927b
jpro <jprodahl@gmail.com>
2022-02-17 13:08:52 -0600

    recursively add children

--- src/index.js
+++ src/index.js
@@ -2,100 +2,124 @@ const React = require("react");
 const ReactDOM = require("react-dom");
 const isPlainObject = require("lodash/isPlainObject");
 const isEqual = require("lodash/isEqual");
+const isEmpty = require("lodash/isEmpty");
 
 function angularize(Component, componentName, angularApp, bindings) {
-	bindings = bindings || {};
-	if (typeof window === "undefined" || typeof angularApp === "undefined") return;
+  bindings = bindings || {};
+  if (typeof window === "undefined" || typeof angularApp === "undefined")
+    return;
 
-	angularApp.component(componentName, {
-		bindings,
-		controller: ["$element", function ($element) {
-			if (window.angular) {
-				// Add $scope
-				this.$scope = window.angular.element($element).scope();
+  angularApp.component(componentName, {
+    bindings,
+    controller: [
+      "$element",
+      function ($element) {
+        if (window.angular) {
+          // Add $scope
+          this.$scope = window.angular.element($element).scope();
 
-				// Create a map of objects bound by '='
-				// For those that exists, use $doCheck to check them using angular.equals and trigger $onChanges
-				const previous = {};
-				this.$onInit = () => {
-					for (let bindingKey of Object.keys(bindings)) {
-						if(/^data[A-Z]/.test(bindingKey)) {
-							console.warn(`'${bindingKey}' binding for ${componentName} component will be undefined because AngularJS ignores attributes starting with data-`);
-						}
+          // Create a map of objects bound by '='
+          // For those that exists, use $doCheck to check them using angular.equals and trigger $onChanges
+          const previous = {};
+          this.$onInit = () => {
+            for (let bindingKey of Object.keys(bindings)) {
+              if (/^data[A-Z]/.test(bindingKey)) {
+                console.warn(
+                  `'${bindingKey}' binding for ${componentName} component will be undefined because AngularJS ignores attributes starting with data-`
+                );
+              }
 
-						if (bindings[bindingKey] === "=") {
-							previous[bindingKey] = window.angular.copy(this[bindingKey]);
-						}
-					}
-				};
+              if (bindings[bindingKey] === "=") {
+                previous[bindingKey] = window.angular.copy(this[bindingKey]);
+              }
+            }
+          };
 
-				this.$doCheck = () => {
-					for (let previousKey of Object.keys(previous)) {
-						if (!equals(this[previousKey], previous[previousKey])) {
-							this.$onChanges();
-							previous[previousKey] = window.angular.copy(this[previousKey]);
-							return;
-						}
-					}
-				}
-			}
+          this.$doCheck = () => {
+            for (let previousKey of Object.keys(previous)) {
+              if (!equals(this[previousKey], previous[previousKey])) {
+                this.$onChanges();
+                previous[previousKey] = window.angular.copy(this[previousKey]);
+                return;
+              }
+            }
+          };
+        }
 
-			this.$onChanges = () => {
-				ReactDOM.render(React.createElement(Component, this), $element[0]);
-			};
-		}]
-	})
+        this.$onChanges = () => {
+          const children = Array.from($element[0].children || []).map((child) =>
+            recursivelyCreateElement(child)
+          );
+          ReactDOM.render(
+            React.createElement(Component, this, ...children),
+            $element[0]
+          );
+        };
+      },
+    ],
+  });
 }
 
 function angularizeDirective(Component, directiveName, angularApp, bindings) {
-	bindings = bindings || {};
-	if (typeof window === "undefined" || typeof angularApp === "undefined") return;
+  bindings = bindings || {};
+  if (typeof window === "undefined" || typeof angularApp === "undefined")
+    return;
 
-	angularApp.directive(directiveName, function () {
-		return {
-			scope: bindings,
-			replace: true,
-			link: function (scope, element) {
-				// Add $scope
-				scope.$scope = scope;
+  angularApp.directive(directiveName, function () {
+    return {
+      scope: bindings,
+      replace: true,
+      link: function (scope, element) {
+        // Add $scope
+        scope.$scope = scope;
 
-				// First render - needed?
-				ReactDOM.render(React.createElement(Component, scope), element[0]);
+        // First render - needed?
+        ReactDOM.render(React.createElement(Component, scope), element[0]);
 
-				// Watch for any changes in bindings, then rerender
-				const keys = [];
-				for (let bindingKey of Object.keys(bindings)) {
-					if(/^data[A-Z]/.test(bindingKey)) {
-						console.warn(`'${bindingKey}' binding for ${directiveName} directive will be undefined because AngularJS ignores attributes starting with data-`);
-					}
-					if (bindings[bindingKey] !== "&") {
-						keys.push(bindingKey);
-					}
-				}
+        // Watch for any changes in bindings, then rerender
+        const keys = [];
+        for (let bindingKey of Object.keys(bindings)) {
+          if (/^data[A-Z]/.test(bindingKey)) {
+            console.warn(
+              `'${bindingKey}' binding for ${directiveName} directive will be undefined because AngularJS ignores attributes starting with data-`
+            );
+          }
+          if (bindings[bindingKey] !== "&") {
+            keys.push(bindingKey);
+          }
+        }
 
-				scope.$watchGroup(keys, () => {
-					ReactDOM.render(React.createElement(Component, scope), element[0]);
-				});
-			}
-		}
-	});
+        scope.$watchGroup(keys, () => {
+          ReactDOM.render(React.createElement(Component, scope), element[0]);
+        });
+      },
+    };
+  });
 }
 
 function getService(serviceName) {
-	if (typeof window === "undefined" || typeof window.angular === "undefined") return {};
-	return window.angular.element(document.body).injector().get(serviceName);
+  if (typeof window === "undefined" || typeof window.angular === "undefined")
+    return {};
+  return window.angular.element(document.body).injector().get(serviceName);
 }
 
 function equals(o1, o2) {
-	// Compare plain objects without equality check that angular.equals does
-	if (isPlainObject(o1) && isPlainObject(o2)) {
-		return isEqual(o1, o2);
-	}
-	return window.angular.equals(o1, o2)
+  // Compare plain objects without equality check that angular.equals does
+  if (isPlainObject(o1) && isPlainObject(o2)) {
+    return isEqual(o1, o2);
+  }
+  return window.angular.equals(o1, o2);
+}
+
+function recursivelyCreateElement(el) {
+  const children = !isEmpty(el.children)
+    ? Array.from(el.children).map((child) => recursivelyCreateElement(child))
+    : el.textContent;
+  return React.createElement(`${el.tagName}`.toLowerCase(), {}, ...children);
 }
 
 module.exports = {
-	getService,
-	angularize,
-	angularizeDirective
+  getService,
+  angularize,
+  angularizeDirective,
 };
